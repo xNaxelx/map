7eddea44989b1404cb5a0ca3beee8b2fc9196f7f
xNaxelx <46362288+xNaxelx@users.noreply.github.com>
2025-07-13 22:50:55 +0300

    git pages

--- noescape/vite.config.js
+++ noescape/vite.config.js
@@ -1,17 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
-  server: {
-    strictPort: true, // API key is restricted to port 5173
-    proxy: {
-        '/data': 'https://noescape.fyi', // Get latest data files from website
-    },
-  },
-  preview: {
-    port: 8080,
-    strictPort: true, // API key is restricted to port 8080
-  },
+  // Укажите здесь имя вашего репозитория на GitHub
+  base: '/map/', // <-- ДОБАВИТЬ ЭТУ СТРОКУ
   plugins: [react()],
 })
